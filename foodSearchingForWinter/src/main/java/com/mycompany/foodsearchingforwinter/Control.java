d22504a33d71b95265207e9d61a503895d04c83a
yya216 <yu_yuan_2@sfu.ca>
2022-10-29 23:35:44 -0700

    Add some comments for JavaDoc

--- foodSearchingForWinter/src/main/java/com/mycompany/foodsearchingforwinter/Control.java
+++ foodSearchingForWinter/src/main/java/com/mycompany/foodsearchingforwinter/Control.java
@@ -3,6 +3,11 @@ package com.mycompany.foodsearchingforwinter;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+/**This class implements keylistener which It listens to the rest of the user's
+ * input from the keyboard, which includes variables representing the four directions
+ * of up, down, left and right. The movement of the character is controlled by pressing or
+ * releasing the four keys representing the up, down, left and right.
+ */
 public class Control implements KeyListener {
     public boolean isUp, isDown, isRight, isLeft, isIdle;
     @Override
@@ -10,6 +15,11 @@ public class Control implements KeyListener {
 
     }
 
+    /**This method will move immediately character  after user press the key which reperenst direction
+     * it take the information which get from the keyboard
+     * @param e the Direction Key entered by user from keyboard
+     * @see The character move after pressing the key
+     */
     @Override
     public void keyPressed(KeyEvent e) {
         int code = e.getKeyCode();
@@ -27,6 +37,11 @@ public class Control implements KeyListener {
         }
     }
 
+    /**This method will stop character action after user release the key, it take the
+     * information which get from the keyboard
+     * @param e the Direction Key entered by user from keyboard
+     * @see The character stop action after releasing the key
+     */
     @Override
     public void keyReleased(KeyEvent e) {
         int code = e.getKeyCode();
