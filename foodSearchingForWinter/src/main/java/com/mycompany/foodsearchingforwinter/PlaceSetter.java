c5df6a375db5c8818172883c03e540c33294385b
mml31 <linda_li_4@sfu.ca>
2022-11-01 22:32:38 -0700

    add more carrot on map

--- foodSearchingForWinter/src/main/java/com/mycompany/foodsearchingforwinter/PlaceSetter.java
+++ foodSearchingForWinter/src/main/java/com/mycompany/foodsearchingforwinter/PlaceSetter.java
@@ -1,9 +1,33 @@
 package com.mycompany.foodsearchingforwinter;
 
+import Reward.Reward_carrot;
+
 public class PlaceSetter {
     GamePanel gp;
 
-    public PlaceSetter(){
+    public PlaceSetter(GamePanel gp){
+        this.gp = gp;
+    }
 
+    public void setThing(){
+        gp.the_rewards[0] = new Reward_carrot();
+        gp.the_rewards[0].x = 6*gp.tileSize;
+        gp.the_rewards[0].y = 9*gp.tileSize;
+
+        gp.the_rewards[1] = new Reward_carrot();
+        gp.the_rewards[1].x = 12*gp.tileSize;
+        gp.the_rewards[1].y = 12*gp.tileSize;
+
+        gp.the_rewards[2] = new Reward_carrot();
+        gp.the_rewards[2].x = 20*gp.tileSize;
+        gp.the_rewards[2].y = 5*gp.tileSize;
+
+        gp.the_rewards[3] = new Reward_carrot();
+        gp.the_rewards[3].x = 24*gp.tileSize;
+        gp.the_rewards[3].y = 14*gp.tileSize;
+
+        gp.the_rewards[4] = new Reward_carrot();
+        gp.the_rewards[4].x = 6*gp.tileSize;
+        gp.the_rewards[4].y = 2*gp.tileSize;
     }
 }
