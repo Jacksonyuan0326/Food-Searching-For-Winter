7221281f8a3214024f3a13ea13b416c32c5f2d09
mml31 <linda_li_4@sfu.ca>
2022-11-01 20:21:16 -0700

    create Reward class and package

---
+++ foodSearchingForWinter/src/main/java/Reward/All_Reward.java
@@ -0,0 +1,10 @@
+package Reward;
+
+import java.awt.image.BufferedImage;
+public class All_Reward {
+    public BufferedImage image;
+    public String name;
+    public boolean collision=false;
+    public int x, y;
+
+}
