a91956bf217d8bb0b09c29c8ab422650b6db6f98
mml31 <linda_li_4@sfu.ca>
2022-11-26 10:17:27 -0800

    door test

---
+++ foodSearchingForWinter/src/test/java/DoorTest.java
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertTrue;
+
+import Reward.*;
+import java.io.IOException;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+
+
+public class DoorTest {
+    
+    @Test
+    void name(){
+        Door d = new Door();
+        assertTrue(d.name == "door");
+        System.out.println("name correct");
+    }
+
+    @Test
+    void collision(){
+        Door d = new Door();
+        assertTrue(d.collision == true);
+        System.out.println("collison correct");
+    }
+
+    @Test
+    void image_load(){
+        try{
+            Door d = new Door();
+            d.image= ImageIO.read(getClass().getResourceAsStream("/Reward/door.png"));
+            System.out.println("image load correct");
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+    
+
+}
